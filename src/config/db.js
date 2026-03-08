6ec06268580d41e8fa1a6b6725f76531137eaac2
Nirob kuri <nirobkuri448@gmail.com>
2026-03-08 16:54:28 +0000

    fixed all code

--- src/config/db.js
+++ src/config/db.js
@@ -1,15 +1,14 @@
 const mongoose = require("mongoose");
-const dns = require("dns");
-dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
 const connectDB = async () => {
-  mongoose.set("strictQuery", false);
-  try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log("connected to database");
-  } catch (err) {
-    console.log("error connecting to database:", err.message);
-  }
+    mongoose.set("strictQuery", false);
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log("connected to database");
+    } catch (err) {
+        console.log("error connecting to database:", err.message);
+        // Remove process.exit(1) so server still runs
+    }
 };
 
 module.exports = connectDB;
